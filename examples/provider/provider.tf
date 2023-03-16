b10ceca14644bdbe551f5a747144ae55604d2ad4
xiaotongwong <wongxiaotong1@gmail.com>
2023-03-16 12:32:52 +0800

    develop-ram-group-resource

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,11 +1,16 @@
 terraform {
   required_providers {
     st-alicloud = {
-      source = "myklst/st-alicloud"
+      source = "example.local/myklst/st-alicloud"
     }
   }
 }
 
 provider "st-alicloud" {
   region = "cn-hongkong"
+}
+
+resource "st-alicloud_alicloud_ram_group_membership" "test" {
+  group_name = "devops-group-02"
+  user_names = "devopsuser02"
 }
