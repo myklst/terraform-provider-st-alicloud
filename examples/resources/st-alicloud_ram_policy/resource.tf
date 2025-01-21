0713c06be9846b4e2228e51dba5775bced344cfe
LiangQuanWong <liangquanwong@ipa.sige.la>
2025-01-21 17:51:54 +0800

    Cleanup example/resource/ram and generate docs

--- examples/resources/st-alicloud_ram_policy/resource.tf
+++ examples/resources/st-alicloud_ram_policy/resource.tf
@@ -1,24 +1,4 @@
-locals {
-  users = ["lq-user-2", "lq-user-3"] //custom users used for testing. Please either create said users or remove them from this list.
-}
-
 resource "st-alicloud_ram_policy" "ram_policy" {
-  for_each = toset(local.users)
-
-  attached_policies = [
-    "AliyunRAMReadOnlyAccess", "AliyunOSSReadOnlyAccess",
-  "LqTestPolicy", "AliyunPubDNSReadOnlyAccess"] //custom policies used for testing. Please either create said policies or remove them from this list.
-  user_name = each.value
-}
-
-terraform {
-  required_providers {
-    st-alicloud = {
-      source = "example.local/myklst/st-alicloud"
-    }
-  }
-}
-
-provider "st-alicloud" {
-  region = "cn-hongkong"
+  user_name = "devopsuser01"
+  attached_policies = ["AliyunRAMReadOnlyAccess", "AliyunOSSReadOnlyAccess", "AliyunPubDNSReadOnlyAccess",]
 }
