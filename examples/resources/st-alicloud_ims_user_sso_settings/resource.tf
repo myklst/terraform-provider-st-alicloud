e5b48bfb9852e08e37a82e743f0401ca8f048e72
sooncheewong <sooncheewong21@gmail.com>
2025-08-15 16:16:51 +0800

    Change domain.

--- examples/resources/st-alicloud_ims_user_sso_settings/resource.tf
+++ examples/resources/st-alicloud_ims_user_sso_settings/resource.tf
@@ -2,5 +2,5 @@ resource "st-alicloud_ims_user_sso_settings" "example" {
   sso_enabled           = true
   metadata_document     = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbxxxxxxxxxxxx"
   sso_login_with_domain = true
-  auxiliary_domain      = "xxx.com"
+  auxiliary_domain      = "example.com"
 }
