0bd20f1fc813dac1a43b63d3a59044559f2d33ef
DeJiar DevOps <devops@dejiar.com>
2026-07-23 00:08:08 +0000

    docs(kvstore): add docs and examples for burst and shard bandwidth resources
    - docs/resources/kvstore_instance_bandwidth_burst.md
    - docs/resources/kvstore_shard_additional_bandwidth.md
    - examples/resources/st-alicloud_kvstore_instance_bandwidth_burst/resource.tf
    - examples/resources/st-alicloud_kvstore_shard_additional_bandwidth/resource.tf

---
+++ examples/resources/st-alicloud_kvstore_instance_bandwidth_burst/resource.tf
@@ -0,0 +1,4 @@
+resource "st-alicloud_kvstore_instance_bandwidth_burst" "default" {
+  instance_id = "r-xxxxx"
+  enabled     = true
+}
---
+++ examples/resources/st-alicloud_kvstore_shard_additional_bandwidth/resource.tf
@@ -0,0 +1,5 @@
+resource "st-alicloud_kvstore_shard_additional_bandwidth" "shard_0" {
+  instance_id = "r-xxxxx"
+  node_id     = "100180258"
+  bandwidth   = 96
+}
